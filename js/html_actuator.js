fcbb0c91a8385e6818a757ec6632481a796e5407
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2021-01-22 14:34:11 -0600

    5702887 6x6

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,8 +57,6 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 2584) classes.push("tile-super");
-
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
